a7554b553566ad0ca8bc46463e8f99073b35abac
lukasz-jakub-adamczuk <lukasz.jakub.adamczuk@gmail.com>
2013-05-30 23:19:50 +0300

    Create Router.php

---
+++ Core/Router.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * podstawowa klasa routera
+ * okresla kontroler i akcje na podstawie url
+ * 
+ * @author ash
+ *
+ */
+class Router {
+
+  /**
+	 * zwraca obiekt kontrolera
+	 * 
+	 * @return Controller $oController
+	 */
+	public static function init() {
+		// sprawdzenie urla
+		// TODO verifiy inputs
+		if (isset($_GET['ctrl'])) {
+			$sController = $_GET['ctrl'];
+		} else {
+			$sController = IVY_DEFAULT_CONTROLLER;
+		}
+		if (isset($_GET['act'])) {
+			$sAction = $_GET['act'];
+		} else {
+			$sAction = IVY_DEFAULT_ACTION;
+		}
+		//echo $sController;
+		//echo $sAction;
+
+		$sControllerName = str_replace(' ', '', ucwords(str_replace('-', ' ', $sController))).'Controller';
+		
+		if (file_exists(CTRL_DIR.DS.$sControllerName.'.php')) {
+			require_once CTRL_DIR.DS.$sControllerName.'.php';
+			$oController = new $sControllerName;
+			
+			$oController->setActionName(ucfirst($sAction)).'Action';
+			
+			$oController->setModelName($oController->getCtrlName().$oController->getActionName());
+			$oController->setViewName($oController->getCtrlName().$oController->getActionName());
+			if (file_exists(TPL_DIR.DS.THEME_DIR.$oController->getCtrlName('lower').'-'.$oController->getActionName('lower').'.tpl')) {
+				$oController->setTemplateName($oController->getCtrlName('lower').'-'.$oController->getActionName('lower'));
+			} else {
+				if (file_exists(TPL_DIR.DS.THEME_DIR.'all-'.$oController->getActionName('lower').'.tpl')) {
+					$oController->setTemplateName('all-'.$oController->getActionName('lower'));
+				} else {
+					$oController->setTemplateName('all-index');
+				}
+			}
+		} else {
+			if (file_exists(CTRL_DIR.DS.'ErrorController.php')) {
+				require_once CTRL_DIR.DS.'ErrorController.php';
+			} else {
+				require_once ASH_LIB_DIR.DS.'Core/ErrorController.php';
+			}
+			$oController = new ErrorController;
+		}
+		//echo $oController;
+
+		$oController->run();
+	}
+}
+
+?>
