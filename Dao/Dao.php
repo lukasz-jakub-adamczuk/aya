a0ea0af381cc976be25a24bb6adf5ae8920af7f3
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2014-02-04 17:51:00 +0100

    adding Dao abstract

---
+++ Dao/Dao.php
@@ -0,0 +1,36 @@
+<?php
+
+class Dao {
+	
+	public static function entity($sName = null, $mIdentifier = 0, $sOwner = null) {
+		if (file_exists(DAO_DIR.'entities/'.$sName.'Entity.php')) {
+			require_once DAO_DIR.'entities/'.$sName.'Entity.php';
+			$sEntity = $sName.'Entity';
+		} else {
+			require_once AYA_DIR.'/Dao/Entity.php';
+			$sEntity = 'Entity';
+			if ($sName) {
+				$sIdLabel = strtolower(preg_replace('/([a-z])([A-Z])/', '$1_$2', $sName));
+			}
+		}
+		if ($sIdLabel) {
+			return new $sEntity($mIdentifier, $sIdLabel);
+		} else {
+			return new $sEntity($mIdentifier);
+		}
+	}
+	
+	public static function collection($sName, $sOwner = null) {
+		if (file_exists(DAO_DIR.'/collections/'.$sName.'Collection.php')) {
+		    //echo 'dao exists...';
+			require_once DAO_DIR.'/collections/'.$sName.'Collection.php';
+			$sCollection = $sName.'Collection';
+		} else {
+		    //echo 'dao not exists...';
+			require_once AYA_DIR.'/Dao/Collection.php';
+			$sCollection = 'Collection';
+		}
+		return new $sCollection($sName, $sOwner);
+	}
+
+}
