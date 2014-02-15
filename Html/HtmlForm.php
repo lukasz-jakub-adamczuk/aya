afacbf0314b6ebc13689f2585d7817153656ce19
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2014-02-16 00:10:36 +0100

    new HtmlForm class

---
+++ Html/HtmlForm.php
@@ -0,0 +1,46 @@
+<?php
+
+class HtmlForm {
+
+	private $_sId;
+
+	private $_sAction;
+	
+	private $_sMethod = 'post';
+	
+	private $_sEnctype;
+	
+	private $_sLang = 'pl';
+
+	private $_sCacheDir = null;
+
+	public function __construct($sId, $aParams = null) {
+		$this->_sId = $sId;
+	}
+
+	public function configure() {
+
+	}
+
+	protected function _init() {
+
+	}
+
+	public function setCacheDir($sCacheDir) {
+		$this->_sCacheDir = $sCacheDir;
+	}
+
+	
+
+	public function render() {
+		return $this->_renderForm();
+	}
+
+	private function _renderForm() {
+		$s = '';
+		$s .= '<form id="'.$this->_sId.'">';
+		$s .= '<input id="" name="" type="text" value="aaa" />';
+		$s .= '</form>';
+		return $s;
+	}
+}
