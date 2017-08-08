6c926248327b9f74f09baa22cf3dda0023f77943
lukasz-jakub-adamczuk <lukasz.jakub.adamczuk@gmail.com>
2017-08-09 00:39:35 +0200

    create folder for exceptions

---
+++ src/Aya/Exception/MissingEntity.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Aya\Helper;
+
+class Text {
+
+    public static function toPascalCase($text) {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $text)));
+    }
+
+    public static function toCamelCase($text) {
+        return strtolower(preg_replace('/([a-z])([A-Z])/', '$1-$2', $text));
+    }
+
+    public static function toLowerCase($text) {
+        return strtolower(preg_replace('/([a-z])([A-Z])/', '$1-$2', $text));
+    }
+}
---
+++ src/Aya/Exception/MissingEntityException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Aya\Exception;
+
+class MissingEntityException extends \RuntimeException {}
